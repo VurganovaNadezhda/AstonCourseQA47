b2bb2a752d344c6f4311625e2f93fb58a13b6764
VurganovaNadezhda <tolpegina1987@list.ru>
2026-04-07 00:07:52 +0300

    add new branch with lessonTwo

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,82 @@
+package org.example;
+
+public class Product {
+    private String name;
+    private String productionDate;
+    private String manufacturer;
+    private String countryOfOrigin;
+    private double price;
+    private boolean isBooked;
+
+    // Конструктор
+    public Product(String name, String productionDate, String manufacturer,
+                   String countryOfOrigin, double price, boolean isBooked) {
+        this.name = name;
+        this.productionDate = productionDate;
+        this.manufacturer = manufacturer;
+        this.countryOfOrigin = countryOfOrigin;
+        this.price = price;
+        this.isBooked = isBooked;
+    }
+
+    // Метод для вывода информации об объекте
+    public void displayInfo() {
+        System.out.println("================================");
+        System.out.println("Название товара: " + name);
+        System.out.println("Дата производства: " + productionDate);
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Страна происхождения: " + countryOfOrigin);
+        System.out.println("Цена: " + price + " руб.");
+        System.out.println("Статус бронирования: " + (isBooked ? "Забронирован" : "Доступен"));
+        System.out.println("================================");
+    }
+
+    // Геттеры и сеттеры
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(String productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    public void setCountryOfOrigin(String countryOfOrigin) {
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+}
